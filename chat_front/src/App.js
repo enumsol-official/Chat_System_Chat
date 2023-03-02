7d727d8be342016570eb804a0dd6be9a1a89942d
noumanali0 <alinouman278@gmail.com>
2023-03-03 00:11:21 +0500

    addedStarterProjectsFrontAndBack

---
+++ chat_front/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 className="text-3xl font-bold underline">Hello</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
